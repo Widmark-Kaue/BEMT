8a420198a211bff317d6bdca9cf028784cbec2e1
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2024-09-16 17:48:06 -0300

    plot airfoil geometry

--- blade_design.py
+++ blade_design.py
@@ -1,2 +1,12 @@
+#%% Packages
 import numpy as np
 import matplotlib.pyplot as plt
+
+#%% Plot airfoil
+x, y = np.loadtxt('S834.dat', skiprows=1, unpack=True)
+
+plt.plot(x, y)
+plt.axis('equal')
+plt.show()
+
+# %%
