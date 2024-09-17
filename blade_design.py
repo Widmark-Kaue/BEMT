18067e090403ec299968c258087af20cc3eb3df5
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2024-09-16 23:35:57 -0300

    cl x alpha and L/d x alpha curves

--- blade_design.py
+++ blade_design.py
@@ -1,12 +1,57 @@
 #%% Packages
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
+from pathlib import Path
+from read_files import process_file
+
+#%% Paths
+image_path = Path('images')
+image_path.mkdir(exist_ok=True)
+
+airfoil_path = Path('airfoil')
 #%% Plot airfoil
-x, y = np.loadtxt('S834.dat', skiprows=1, unpack=True)
+x, y = np.loadtxt(airfoil_path.joinpath('S834.dat'), skiprows=1, unpack=True)
 
 plt.plot(x, y)
 plt.axis('equal')
+plt.grid()
+plt.savefig(image_path.joinpath('airfoil.png'), format = 'png', dpi = 720)
 plt.show()
 
+# %% Load c_lft file and plot Cl x alpha
+c_lift = process_file(airfoil_path.joinpath('s834_c_lft.txt'))
+cols = c_lift.columns
+
+for col in cols:
+    re = float(col.split()[-1])
+    plt.plot(c_lift[col][0][:,  0], c_lift[col][0][:, 1], label=f'Re = {re:.3e}')
+
+plt.title(r'Cl x $\alpha$')
+plt.xlabel(r'$\alpha$ [deg]')
+plt.ylabel('Cl')
+
+plt.legend()
+plt.grid()
+plt.savefig(image_path.joinpath('cl_alpha.png'), format = 'png', dpi = 720)
+plt.show()
+
+# %% Load c_drg file and plot Cl/Cd x alpha
+c_lift_drag = process_file(airfoil_path.joinpath('s834_c_drg.txt'))
+cols = c_lift_drag.columns
+
+for col in cols:
+    re = float(col.split()[-1])
+    LD = c_lift_drag[col][0][:, 1]/c_lift_drag[col][0][:, 2]
+    plt.plot(c_lift_drag[col][0][:,  0], LD , label=f'Re = {re:.3e}')
+
+plt.title(r'L/D x $\alpha$')
+plt.xlabel(r'$\alpha$ [deg]')
+plt.ylabel('Cl/Cd')
+
+plt.legend()
+plt.grid()
+plt.savefig(image_path.joinpath('LD_alpha.png'), format = 'png', dpi = 720)
+plt.show()
 # %%
