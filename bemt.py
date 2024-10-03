4a824e5c03cbe680ea9b1fa2d363a30857c4c790
Widmark-Kaue <widmarkkaue.s.c@gmail.com>
2024-10-03 18:58:06 -0300

    BUG: axial and tangential induction factors are showing negative values

--- bemt.py
+++ bemt.py
@@ -26,7 +26,7 @@ def coefficients_extrapolation(df_coeff:pd.DataFrame, smooth:bool = False) -> pd
     )    
     return  df_extra
 
-def bemt(TSR:np.ndarray, rotor:pd.DataFrame, airfoil_name:str, number_of_blades: int, threeD_correction: bool = False, tip_correction_model:str = 'Prandtl', iter:int = 100, tol:float = 1e-3):
+def bemt(TSR:float, rotor:pd.DataFrame, airfoil_name:str, number_of_blades: int, threeD_correction: bool = False, tip_correction_model:str = 'Prandtl', iter:int = 100, tol:float = 1e-3):
     
     # Read  airfoil data
     c_lift_drag = process_file(airfoil_path.joinpath(f'{airfoil_name}_c_drg.txt'))
@@ -38,29 +38,34 @@ def bemt(TSR:np.ndarray, rotor:pd.DataFrame, airfoil_name:str, number_of_blades:
     Cd_interp = interp1d(np.deg2rad(coeff_extra['alpha']),  coeff_extra['Cd'], kind='cubic')
 
     
-    
+    # Step 0 - Prepare geometric parameters (Using intermediate points to avoid edge effects)
+    r_R = rotor['r/R'].to_numpy()[1:-1]
+    sigma = rotor['sigma'].to_numpy()[1:-1]
+    theta = np.deg2rad(rotor['theta'].to_numpy()[1:-1])
+    x = r_R*TSR                   
+
     # Step 1 - Initialize the BEMT parameters
-    a =  np.zeros(len(rotor['x']))
-    a_line = np.zeros(len(rotor['x']))
-    a_new =  np.zeros(len(rotor['x']))
-    a_line_new = np.zeros(len(rotor['x']))
+    a =  np.zeros(len(x))
+    a_line = np.zeros(len(x))
+    a_new =  np.zeros(len(x))
+    a_line_new = np.zeros(len(x))
     
-    phi = np.zeros(len(rotor['x']))
-    alpha = np.zeros(len(rotor['x']))
-    Ct = np.zeros(len(rotor['x']))
-    Cn = np.zeros(len(rotor['x']))
+    phi = np.zeros(len(x))
+    alpha = np.zeros(len(x))
+    Ct = np.zeros(len(x))
+    Cn = np.zeros(len(x))
+
     error = []
-    
     
     for i in range(iter):
         # Step 2 - Compute flow angle
-        phi = np.arctan((1-a)/(1+a_line)/rotor['x'].to_numpy())
+        phi = np.arctan((1-a)/(1+a_line)/x)
         
         # Step 2.5 - Tip Correction
-        F = tip_correction(phi, rotor['x'].to_numpy()[-1]/rotor['x'].to_numpy(),number_of_blades, model= tip_correction_model)
+        F = tip_correction(phi,1/r_R, number_of_blades, model= tip_correction_model)
         
         # Step 3 - Compute local angle of attack
-        alpha = phi - np.deg2rad(rotor['theta'].to_numpy())
+        alpha = phi - theta 
         
         # Step 4 - Compute local lift and drag coefficients
         Cl = Cl_interp(alpha)
@@ -71,24 +76,39 @@ def bemt(TSR:np.ndarray, rotor:pd.DataFrame, airfoil_name:str, number_of_blades:
         Ct = Cl*np.sin(phi) - Cd*np.cos(phi)
         
         # Step 6 - Update induction factors
-        if min(a) <= 1/3:
-            a_new = 1/((4*F * np.sin(phi)**2/(rotor['sigma'].to_numpy() * Cn)) + 1)
-            a_line_new = 1/((4*F * np.sin(phi)* np.cos(phi)/(rotor['sigma'].to_numpy() * Cn)) - 1)
+        if np.all(a <= 1/3):
+            a_new = 4*F *np.sin(phi)**2/(sigma * Cn) + 1
+            a_new = 1/a_new
+            
+            a_line_new = 4*F*np.sin(phi) *  np.cos(phi)/(sigma * Ct) - 1
+            a_line_new = 1/a_line_new
         else:
-            K = rotor['sigma'].to_numpy()*Cn/(np.sin(phi)**2)
-            func = lambda a: -K + a*(1+ 4*F + 2*K) - a**2*(5*F + K)+3*F*a**3
-            a_new = root(func, 1/3*np.ones(len(a)))
-            a_line_new = (1 - 3*a)/(4*a - 1)
+            locs = a <=  1/3
+            locs_root = a > 1/3
+            
+            a_new[locs] = 1/((4*F[locs] * np.sin(phi[locs])**2/(sigma[locs] * Cn[locs])) + 1)
+            a_line_new[locs] = 1/((4*F[locs] * np.sin(phi[locs])* np.cos(phi[locs])/(sigma[locs] * Ct[locs])) - 1)
+            
+            K = sigma[locs_root]*Cn[locs_root]/(np.sin(phi[locs_root])**2)
+            func = lambda a: -K + a*(1+ 4*F[locs_root] + 2*K) - a**2*(5*F[locs_root] + K)+3*F[locs_root]*a**3
+            sol = root(func, (2/3)*np.ones(len(a[locs_root])))
+            a_new[locs_root] = sol.x
+            a_line_new[locs_root] = (1 - 3*a_new[locs_root])/(4*a_new[locs_root] - 1)
         
         #  Step 7 - Check convergence: Root Mean Square Error
         error.append(np.sqrt(np.sum((a_new - a)**2)/len(a)))
+        print(f'a_new = {len(a_new[a_new < 0])}')
+        print(f'a_line_new ={len(a_line_new[a_line_new < 0])}')
         if error[-1] < tol:
             break
         else:
-            a = a_new
-            a_line = a_line_new
+            a = a_new.copy()
+            a[a < 0] = 0
+            a_line = a_line_new.copy()
+            a_line[a_line  < 0] = 0
 
+    pass
 if __name__ == '__main__':
     airfoil_name = 's834'
-    rotor = blade_design('s834', 7, 2, number_of_sections=280,plot=False)
-    bemt(rotor, airfoil_name, 2)
+    rotor = blade_design('s834', 7, 2, number_of_sections=50,plot=False)
+    bemt(6, rotor, airfoil_name, 2, iter=500)
